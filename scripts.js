9f65f1f4e185a64d1701f4eb450cf2bd7c50fde3
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-06 17:00:57 -0400

    Script file: trying to add a button with functionality on how to display the choosen drink to displays it's instruction

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const searchButton = document.querySelector('#search');
 const answer = document.querySelector('#blank');
 
+
 function barAndDrinks(e) {
   let input = answer.value;
   //console.log('Inside barAndDrinks')
@@ -33,33 +34,41 @@ function displayTheInfo(drinks) {
   for (let i = 0; i < drinks.length; i++) {
     console.log('Name:', drinks[i].strDrink);
     console.log('Ingredients:');
+
+    //Creating DIV for each resulted drink
     const divDrink = document.createElement('DIV');
 
+    //create the name
     const name = document.createElement('h4');
     name.innerHTML = drinks[i].strDrink;
     divDrink.append(name);
 
     //Here create arrays for the ingredient list and measurements
-    const ingredientList = getIngredients(drinks[i]);
-    const measurementList = getMeasurments(drinks[i]);
-    ingredientList.forEach((ingredient, idx) => {
-      console.log(`Ingredient ${idx + 1}: ${ingredient} Measurements ${idx + 1}: ${measurementList[idx]}`);
+    // const ingredientList = getIngredients(drinks[i]);
+    // const measurementList = getMeasurments(drinks[i]);
+    // ingredientList.forEach((ingredient, idx) => {
+    //   console.log(`Ingredient ${idx + 1}: ${ingredient} Measurements ${idx + 1}: ${measurementList[idx]}`);
 
-    })
+    // })
 
 
-    console.log('Instructions:', drinks[i].strInstructions);
+    // console.log('Instructions:', drinks[i].strInstructions);
+    // console.log('Image:', drinks[i].strDrinkThumb);
 
-    console.log('Image:', drinks[i].strDrinkThumb);
-
+    //Creating the image element
     const imageSrc = document.createElement('img');
     imageSrc.setAttribute('src', drinks[i].strDrinkThumb + '/preview');
     imageSrc.setAttribute('alt', drinks[i].strDrink);
     console.log(imageSrc);
     divDrink.append(imageSrc);
 
+
+    //Creating the button element
     const choosenButton = document.createElement('button');
     choosenButton.innerHTML = "This one";
+    choosenButton.setAttribute('id', `${drinks[i].idDrink}`);
+    //choosenButton.onclick = "choosenDrink('1')";
+    choosenButton.setAttribute('onclick', "choosenDrink('1')");
     divDrink.append(choosenButton);
 
     //Adding the divDrank to the section - this will appened to the page
@@ -95,5 +104,11 @@ function getMeasurments(drinkObject) {
     }
   }
   return measurementList;
+}
+
+
+function choosenDrink(data) {
+  alert("I was clicked");
+  console.log(data);
 }
 searchButton.addEventListener('click', barAndDrinks);
