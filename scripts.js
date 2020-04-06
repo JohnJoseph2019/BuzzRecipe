a647990b61393b3a5a669b652adfe8c0c22d4d95
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-06 12:35:41 -0400

    Script file - Added getMeasurments function to help the displayTheInfo funciton with the measurments- Also in the displauTheInfo function I console log the instructions and the image url

--- scripts.js
+++ scripts.js
@@ -27,24 +27,43 @@ function displayTheInfo(drinks) {
     console.log('Name:', drinks[i].strDrink);
     console.log('Ingredients:');
     const ingredientList = getIngredients(drinks[i]);
-    ingredientList.forEach(ingredient => {
-      console.log(ingredient);
+    const measurementList = getMeasurments(drinks[i]);
+    ingredientList.forEach((ingredient, idx) => {
+      console.log(`Ingredient ${idx + 1}: ${ingredient} Measurements ${idx + 1}: ${measurementList[idx]}`);
+
     })
+    console.log('Instructions:', drinks[i].strInstructions);
+    console.log('Image:', drinks[i].strDrinkThumb);
 
   }
 }
-//To get the inngredients
+//To get list of inngredients
 function getIngredients(drinkObject) {
   console.log('Inside getIngredients:');
   console.log(drinkObject);
   let ingredientList = [];
   let counter = 1;
-  for (key in drinkObject) {
-    if (key === `strIngredient${counter}` && drinkObject[key] !== null) {
-      ingredientList.push(drinkObject[key]);
+  for (ingredient in drinkObject) {
+    if (ingredient === `strIngredient${counter}` && drinkObject[ingredient] !== null) {
+      ingredientList.push(drinkObject[ingredient]);
       counter++;
     }
   }
   return ingredientList;
+}
+
+//To get the list of Measurments:
+function getMeasurments(drinkObject) {
+  console.log('Inside getMeasurments:');
+  console.log(drinkObject);
+  let measurementList = [];
+  let counter = 1;
+  for (measurement in drinkObject) {
+    if (measurement === `strMeasure${counter}` && drinkObject[measurement] !== null) {
+      measurementList.push(drinkObject[measurement]);
+      counter++;
+    }
+  }
+  return measurementList;
 }
 searchButton.addEventListener('click', barAndDrinks);
