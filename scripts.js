84dcde4ac776cfc9213d287a71c6053fef6b3ab5
Juan Delgado <Delgado-juan@hotmail.com>
2020-04-06 12:09:02 -0400

    Script file - Added displayTheInfo that will add data to the page, but for right is console loggin . created getIngredients function so that it can get the indredients of the drink and return it to the displayTheinfo as an array so it can be use to append to the page. Right now console loggin the info

--- scripts.js
+++ scripts.js
@@ -4,20 +4,47 @@ const searchButton = document.querySelector('#search');
 const answer = document.querySelector('#blank');
 
 function barAndDrinks(e) {
-  let drink = answer.value;
-  console.log('We in the searchDrinks function')
-  console.log('I search for: ', drink)
-  searchDrinks(drink);
+  let input = answer.value;
+  //console.log('Inside barAndDrinks')
+  //console.log('I search for:', input)
+  searchDrinks(input);
 }
 
 async function searchDrinks(name) {
+  console.log('Inside searchDrinks');
   let searchDrink = name;
   const response = await axios.get(BASE_URL + searchDrink);
-  console(response);
-  console(response.data);
-  console(response.data.drinks);
+  //console.log(response);
+  //console.log(response.data);
   displayTheInfo(response.data.drinks);
 }
+//displaying the items
+function displayTheInfo(drinks) {
+  console.log('Inside displayTheInfo:');
+  console.log(drinks);
 
+  for (let i = 0; i < drinks.length; i++) {
+    console.log('Name:', drinks[i].strDrink);
+    console.log('Ingredients:');
+    const ingredientList = getIngredients(drinks[i]);
+    ingredientList.forEach(ingredient => {
+      console.log(ingredient);
+    })
 
+  }
+}
+//To get the inngredients
+function getIngredients(drinkObject) {
+  console.log('Inside getIngredients:');
+  console.log(drinkObject);
+  let ingredientList = [];
+  let counter = 1;
+  for (key in drinkObject) {
+    if (key === `strIngredient${counter}` && drinkObject[key] !== null) {
+      ingredientList.push(drinkObject[key]);
+      counter++;
+    }
+  }
+  return ingredientList;
+}
 searchButton.addEventListener('click', barAndDrinks);
